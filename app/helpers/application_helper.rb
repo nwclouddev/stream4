c84c49b7e547a515024a475ac8f7a59bb38f235b
Adam Johnson <adam@nwclouddev.com>
2024-01-01 13:55:10 -0800

    Add Release ID/Env to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def release_id
+    case Rails.env
+    when 'development'
+      env_shorthand = 'DEV'
+    when 'production'
+      env_shorthand = 'PROD'
+    when 'test'
+      env_shorthand = 'TEST'
+    else
+      env_shorthand = 'OTHER'
+    end
+    if ENV['DEPLOYMENT_ID'] == nil
+      env_shorthand + ' | nil'
+    else
+      env_shorthand + ' | ' + ENV['DEPLOYMENT_ID'].to_s
+    end
+  end
 end
