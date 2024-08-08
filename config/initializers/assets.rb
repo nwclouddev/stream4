47501c2d1e58be6bc76e24f5aafe053c65aec5ab
ADAM JOHNSON <adam@nwclouddev.com>
2024-08-08 09:34:09 -0700

    Add assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,10 +8,8 @@ Rails.application.config.assets.version = '2.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
-if Rails.env == 'development'
-  Rails.application.config.assets.paths << Rails.root.join('/media/adam/')
-  Rails.application.config.assets.paths << Rails.root.join('/Volumes/home/')
-end
+Rails.application.config.assets.paths << Rails.root.join('/mnt/NAS/Utility/')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
