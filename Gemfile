ac8a302f6cae4dad0bb128641f1a98350dd98e63
Adam Johnson <adam@nwclouddev.com>
2024-10-16 13:56:27 -0700

    Upgrade Kamal

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@
 
 source 'https://rubygems.org'
 
-ruby '3.3.4'
+ruby '3.3.5'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.2.0.beta2'
+gem 'rails', '~> 8.0.0.beta1'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,6 +7,8 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
   if ENV["CAPYBARA_SERVER_PORT"]
     served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
 
     driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
       browser: :remote,
@@ -29,6 +31,42 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     }
   else
     driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  end
+
+  end
+
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
+    }
+  else
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  end
+
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
+    }
+  else
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  end
+
   end
 
   end
